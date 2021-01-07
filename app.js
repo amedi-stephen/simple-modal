b2bc05dc176928ea7e615241784bd17ce82e3e8f
steve <amsteve46@gmail.com>
2021-01-07 18:10:30 +0300

    The complete modal project with html, css and javascript

---
+++ app.js
@@ -0,0 +1,17 @@
+const modal = document.querySelector("#modalSection");
+const closeBtn = document.querySelector(".closeBtn");
+const btn = document.querySelector("#btnModal");
+
+btn.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+});
